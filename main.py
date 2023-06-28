47d4aeba41a1d4a9dfc1a4aacaa6f999609d6c88
StevenMunozTufino <steven.munoz@ucuenca.edu.ec>
2023-06-28 08:39:52 -0500

    manejoDeDesconexion

--- main.py
+++ main.py
@@ -7,14 +7,18 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = os.urandom(24)
 socketio = SocketIO(app)
 
-# Configura la conexión con RabbitMQ
-credentials = pika.PlainCredentials('user', 'QF1DCB!GYWpJ')
-parameters = pika.ConnectionParameters(host='20.232.116.211', credentials=credentials)
-connection = pika.BlockingConnection(parameters)
-channel = connection.channel()
+# Variables globales
+connection = None
+channel = None
 
-# Declara una cola para recibir mensajes
-channel.queue_declare(queue='my_queue')
+# Configura la conexión con RabbitMQ
+def connect_rabbitmq():
+    global connection, channel
+    credentials = pika.PlainCredentials('user', 'QF1DCB!GYWpJ')
+    parameters = pika.ConnectionParameters(host='20.232.116.211', credentials=credentials)
+    connection = pika.BlockingConnection(parameters)
+    channel = connection.channel()
+    channel.queue_declare(queue='my_queue')
 
 @app.route('/')
 def index():
@@ -22,9 +26,17 @@ def index():
 
 @socketio.on('message')
 def handle_message(message):
-    # Envía el mensaje a la cola de RabbitMQ
-    channel.basic_publish(exchange='', routing_key='my_queue', body=message)
-    emit('message', message, broadcast=True)
+    try:
+        # Verifica si la conexión con RabbitMQ está abierta
+        if not connection or not connection.is_open:
+            connect_rabbitmq()  # Intenta reconectarse si no hay conexión o la conexión está cerrada
+        
+        # Envía el mensaje a la cola de RabbitMQ
+        channel.basic_publish(exchange='', routing_key='my_queue', body=message)
+        emit('message', message, broadcast=True)
+    except pika.exceptions.AMQPConnectionError:
+        print("Error: No se pudo conectar a RabbitMQ.")
 
 if __name__ == '__main__':
+    connect_rabbitmq()  # Establece la conexión al iniciar el programa
     socketio.run(app)

f1f4ebfb661159ef820dd7dec9b421f49ef1b759
Steven <steven.munoz@ucuenca.edu.ec>
2023-06-30 11:16:30 -0500

    p

--- app.py
+++ app.py
@@ -2,14 +2,17 @@ import os
 import pika
 from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
+import threading
+from queue import Queue
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.urandom(24)
-socketio = SocketIO(app,cors_allowed_origins='*')
+socketio = SocketIO(app, cors_allowed_origins='*')
 
 # Variables globales
 connection = None
 channel = None
+message_queue = Queue()  # Cola para almacenar los mensajes recibidos
 
 # Configura la conexión con RabbitMQ
 def connect_rabbitmq():
@@ -19,6 +22,11 @@ def connect_rabbitmq():
     connection = pika.BlockingConnection(parameters)
     channel = connection.channel()
     channel.queue_declare(queue='my_queue')
+
+def callback(ch, method, properties, body):
+    message = body.decode()
+    print("Mensaje recibido: " + message)
+    message_queue.put(message)  # Almacena el mensaje en la cola
 
 @app.route('/')
 def index():
@@ -33,10 +41,31 @@ def handle_message(message):
         
         # Envía el mensaje a la cola de RabbitMQ
         channel.basic_publish(exchange='', routing_key='my_queue', body=message)
-        emit('message', message, broadcast=True)
     except pika.exceptions.AMQPConnectionError:
         print("Error: No se pudo conectar a RabbitMQ.")
 
+def send_queued_messages():
+    while True:
+        message = message_queue.get()  # Obtiene un mensaje de la cola
+        socketio.emit('message', message)  # Envía el mensaje a los clientes conectados
+
+def start_consuming():
+    print("Consumiendo mensajes...")
+    channel.basic_consume(queue='my_queue', on_message_callback=callback, auto_ack=True)
+    channel.start_consuming()
+
 if __name__ == '__main__':
     connect_rabbitmq()  # Establece la conexión al iniciar el programa
+    
+    # Crea un hilo para el consumo de mensajes
+    consuming_thread = threading.Thread(target=start_consuming)
+    consuming_thread.daemon = True  # El hilo se detendrá cuando el programa principal se cierre
+    consuming_thread.start()
+    
+    # Crea un hilo para enviar los mensajes encolados
+    send_messages_thread = threading.Thread(target=send_queued_messages)
+    send_messages_thread.daemon = True  # El hilo se detendrá cuando el programa principal se cierre
+    send_messages_thread.start()
+    
+    # Ejecuta el socket en el hilo principal
     socketio.run(app)

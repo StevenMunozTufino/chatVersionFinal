0592374d989601b51150e3355195ae83a69ac3de
StevenMunozTufino <steven.munoz@ucuenca.edu.ec>
2023-07-04 21:23:44 -0500

    FinalFinal

--- app.py
+++ app.py
@@ -82,21 +82,17 @@ def handle_message(message):
         print("Mensaje de error real:", str(e))
 
 
-# def send_queued_messages():
-#     while True:
-#         message = message_queue.get()  # Obtiene un mensaje de la cola
-        
 
 def start_consuming():
-    while True:
-        try:
-            print("Consumiendo mensajes...")
-            channelRecibir.basic_consume(queue='alexander', on_message_callback=callback, auto_ack=True)
-            channelRecibir.start_consuming()
-        except pika.exceptions.AMQPConnectionError as e:
-            print("Error de conexión RabbitMQ:", str(e))
-            # Intenta reconectarse
-            connect_rabbitmqRecibir()
+
+    try:
+        print("Consumiendo mensajes...")
+        channelRecibir.basic_consume(queue='alexander', on_message_callback=callback, auto_ack=True)
+        channelRecibir.start_consuming()
+    except pika.exceptions.AMQPConnectionError as e:
+        print("Error de conexión RabbitMQ:", str(e))
+        # Intenta reconectarse
+        connect_rabbitmqRecibir()
 
 if __name__ == '__main__':
     socketio.run(app)

8fbee0ca036d353a8a76400c24860f038df49036
StevenMunozTufino <steven.munoz@ucuenca.edu.ec>
2023-07-07 12:16:21 -0500

    Prueba

--- static/js/script.js
+++ static/js/script.js
@@ -1,6 +1,42 @@
+// Obtener el elemento select del ComboBox
+var select = document.getElementById("animal-select");
+var enviar = 'Leon';
+
+// Obtener los elementos de la cabecera a actualizar
+var animalImg = document.getElementById("animal-img");
+var chatTitle = document.getElementById("chat-title");
+
+// Agregar un evento de cambio al select
+select.addEventListener("change", function() {
+  // Obtener el valor seleccionado
+  var selectedAnimal = select.value;
+  enviar = selectedAnimal
+  
+  // Actualizar la imagen y el título de la cabecera según el animal seleccionado
+  switch (selectedAnimal) {
+    case "Leon":
+      animalImg.src = "../static/images/leon.png";
+      chatTitle.textContent = "Chat con León (Server 2)";
+      break;
+    case "Mapache":
+      animalImg.src = "../static/images/mapache.png";
+      chatTitle.textContent = "Chat con Mapache (Server 2)";
+      break;
+    case "Zorro":
+      animalImg.src = "../static/images/zorro.png";
+      chatTitle.textContent = "Chat con Zorro (Server 2)";
+      break;
+    default:
+      animalImg.src = "../static/images/grupo.png";
+      chatTitle.textContent = "Chat en línea (Server 2)";
+      break;
+  }
+});
+
+
 var socket = io();
+
 let numMensajes = 0;
-var idUser = null;
 
 // Función para mostrar el modal al cargar la página
 document.addEventListener('DOMContentLoaded', function() {
@@ -11,19 +47,17 @@ document.addEventListener('DOMContentLoaded', function() {
     $('#modalID').modal('show');
 });
 
-function guardarID() {
+let idUser = null;
 
-    var inputID = document.getElementById('inputID');
-    var idIngresado = inputID.value.trim();
-
-    if (idIngresado !== "") {
-        idUser = idIngresado;
-        console.log("ID ingresado:", idUser);
-
-        // Cierra el modal después de guardar el ID
-        $('#modalID').modal('hide');
-    }
+function guardarID(nombrePerfil) {
+    idUser = nombrePerfil;
+    socket.emit('usuario',idUser);
+    console.log("Perfil seleccionado:", idUser);
+    // Cierra el modal después de guardar el perfil
+    $('#modalID').modal('hide');
 }
+
+
 
 function fecha(){
     var fechaActual = new Date();
@@ -48,28 +82,13 @@ function sendMessage() {
     var message = document.getElementById('message').value;
     if (message.trim() !== '') {
         mensaje = message+'@'+fecha()+ '@'+ horaH() + '@' + idUser;
-        socket.emit('message', mensaje); //Envía a la cola
+        socket.emit('message', {'profile': idUser, 'enviarA': enviar, 'message': mensaje}); //Envía a la cola
+
         document.getElementById('message').value = '';
         document.getElementById('message').focus();
-    }
-}
 
-socket.on('connect', function() {
-    console.log('Connected');
-  });
+        var datos = message.split('@');
 
-  socket.on('disconnect', function() {
-    console.log('Disconnected');
-  });
-
-//WebSocket para escuchar los mensajes recibidos
-socket.on('message', function(message) {
-    numMensajes ++;
-    const numeroElemento = document.getElementById('numero');
-    numeroElemento.textContent = numMensajes + ' Mensajes';
-    //Descomponer el mensaje
-    var datos = message.split('@');
-    if (datos[3] == idUser) {
         console.log("Mensaje Enviado");
         var chatBox = document.getElementById('chat-box');
         var messageContainer = document.createElement('div');
@@ -79,13 +98,18 @@ socket.on('message', function(message) {
         textContainer.classList.add('msg_cotainer_send');
         textContainer.textContent = datos[0];
 
+        var nameElement = document.createElement('span');
+        nameElement.classList.add('msg_name');
+        nameElement.textContent = 'Enviado a '+ enviar;
+
         var timeElement = document.createElement('span');
         timeElement.classList.add('msg_time_send');
-        timeElement.textContent = datos[2]+', ' + datos[1]; // Aquí puedes agregar la hora actual del mensaje
+        timeElement.textContent = datos[2] +', ' + datos[1]; // Aquí puedes agregar la hora actual del mensaje
 
         var imgContainer = document.createElement('div');
         imgContainer.classList.add('img_cont_msg');
 
+        textContainer.appendChild(nameElement);
         var imgElement = document.createElement('img');
         imgElement.src = "../static/images/enviado.png";
         imgElement.classList.add('user_img_msg', 'img_no_border');
@@ -97,40 +121,59 @@ socket.on('message', function(message) {
         chatBox.appendChild(messageContainer);
 
         chatBox.scrollTop = chatBox.scrollHeight;
-    } else {
-        console.log("Mensaje Recibido");
-        var chatBox = document.getElementById('chat-box');
-        var messageContainer = document.createElement('div');
-        messageContainer.classList.add('d-flex', 'justify-content-start', 'mb-4');
-
-        var imgContainer = document.createElement('div');
-        imgContainer.classList.add('img_cont_msg');
-
-        var imgElement = document.createElement('img');
-        imgElement.src = "../static/images/recibido.png";
-        imgElement.classList.add('user_img_msg', 'img_no_border');
-
-        var nameElement = document.createElement('span');
-        nameElement.classList.add('msg_name');
-        nameElement.textContent = datos[3];
-
-        var textContainer = document.createElement('div');
-        textContainer.classList.add('msg_cotainer');
-        textContainer.textContent = datos[0];
-
-        var timeElement = document.createElement('span');
-        timeElement.classList.add('msg_time');
-        timeElement.textContent = datos[2]+', ' + datos[1]; // Aquí puedes agregar la hora actual del mensaje
-
-        textContainer.appendChild(nameElement);
-        textContainer.appendChild(timeElement);
-        imgContainer.appendChild(imgElement);
-        messageContainer.appendChild(imgContainer);
-        messageContainer.appendChild(textContainer);
-        chatBox.appendChild(messageContainer);
-
-        chatBox.scrollTop = chatBox.scrollHeight;
     }
+}
+
+socket.on('connect', function() {
+    console.log('Conectado al servidor');
+});
+
+socket.on('disconnect', function() {
+    console.log('Desconectado del servidor');
+});
+
+//WebSocket para escuchar los mensajes recibidos
+socket.on('message', function(message) {
+
+    console.log(message);
+    numMensajes ++;
+    const numeroElemento = document.getElementById('numero');
+    numeroElemento.textContent = numMensajes + ' Mensajes';
+    //Descomponer el mensaje
+    var datos = message.split('@');
+    
+    console.log("Mensaje Recibido");
+    var chatBox = document.getElementById('chat-box');
+    var messageContainer = document.createElement('div');
+    messageContainer.classList.add('d-flex', 'justify-content-start', 'mb-4');
+
+    var imgContainer = document.createElement('div');
+    imgContainer.classList.add('img_cont_msg');
+
+    var imgElement = document.createElement('img');
+    imgElement.src = "../static/images/recibido.png";
+    imgElement.classList.add('user_img_msg', 'img_no_border');
+
+    var nameElement = document.createElement('span');
+    nameElement.classList.add('msg_name');
+    nameElement.textContent = datos[3];
+
+    var textContainer = document.createElement('div');
+    textContainer.classList.add('msg_cotainer');
+    textContainer.textContent = datos[0];
+
+    var timeElement = document.createElement('span');
+    timeElement.classList.add('msg_time');
+    timeElement.textContent = datos[2]+', ' + datos[1]; // Aquí puedes agregar la hora actual del mensaje
+
+    textContainer.appendChild(nameElement);
+    textContainer.appendChild(timeElement);
+    imgContainer.appendChild(imgElement);
+    messageContainer.appendChild(imgContainer);
+    messageContainer.appendChild(textContainer);
+    chatBox.appendChild(messageContainer);
+
+    chatBox.scrollTop = chatBox.scrollHeight;
 });
 
 document.getElementById('send-button').addEventListener('click', sendMessage);

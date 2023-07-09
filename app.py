9196beaccfd55beb1fb072f33586716df38d8a62
Steven <steven.munoz@ucuenca.edu.ec>
2023-07-09 06:50:16 -0500

    VersionFinal2

--- app.py
+++ app.py
@@ -1,24 +1,17 @@
 import os
 import pika
 from flask import Flask, render_template
-from flask_socketio import SocketIO, emit
-import threading
+from flask_socketio import SocketIO
 from flask_cors import CORS
+from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
-# app.logger.setLevel(logging.WARNING)
 app.config['SECRET_KEY'] = os.urandom(24)
 socketio = SocketIO(app,cors_allowed_origins='*')
 CORS(app)
 
-consuming_thread = None
-
-# Variables globales
 connection = None
-connectionRecibir = None
 channel = None
-channelRecibir = None
-#message_queue = Queue()  # Cola para almacenar los mensajes recibidos
 
 # Configura la conexión con RabbitMQ
 def connect_rabbitmq():
@@ -27,80 +20,61 @@ def connect_rabbitmq():
     parameters = pika.ConnectionParameters(host='20.232.116.211', credentials=credentials)
     connection = pika.BlockingConnection(parameters)
     channel = connection.channel()
-    channel.queue_declare(queue='steven')
-
-def connect_rabbitmqRecibir():
-    global connectionRecibir, channelRecibir
-    credentials = pika.PlainCredentials('user', 'QF1DCB!GYWpJ')
-    parameters = pika.ConnectionParameters(host='20.232.116.211', credentials=credentials)
-    connectionRecibir = pika.BlockingConnection(parameters)
-    channelRecibir = connectionRecibir.channel()
-    channelRecibir.queue_declare(queue='alexander')
 
 
-def callback(ch, method, properties, body):
-    message = body.decode()
-    print("Mensaje recibido: " + message)
-    socketio.emit('message', message)  # Envía el mensaje a los clientes conectados
-    #message_queue.put(message)  # Almacena el mensaje en la cola
+
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+
+@socketio.on('usuario')
+def handle_login(id):
+    pass
+   
+
+#Conexion y desconexión de clientes
+#####################################################################################################################
+
 @socketio.on('connect')
 def handle_connect():
-    global consuming_thread
-    connect_rabbitmqRecibir()
-    consuming_thread = threading.Thread(target=start_consuming)
-    consuming_thread.daemon = True  # El hilo se detendrá cuando el programa principal se cierre
-    consuming_thread.start()
+    connect_rabbitmq()  # Intenta conectarse a RabbitMQ
 
 @socketio.on('disconnect')
 def handle_disconnect():
-    global consuming_thread
-    global connectionRecibir
-    global connection
-    connectionRecibir.close()  # Cierra la conexión con RabbitMQ
-    connection.close()
-    if consuming_thread is not None:
-        connectionRecibir.close()  # Cierra la conexión con RabbitMQ
-        connection.close()
-        consuming_thread.join()  # Detener el hilo si existe
-        consuming_thread = None
+    global connection, channel
 
+    if connection and connection.is_open:
+        connection.close()
+   
 
 @socketio.on('message')
-def handle_message(message):
-
+def handle_message(data):
+    mensaje=data['message']
+    enviarA=data['enviarA']
+    print("enviarA: ",enviarA)
     try:
-
         # Verifica si la conexión con RabbitMQ está abierta
         if not connection or not connection.is_open:
-
             connect_rabbitmq()  # Intenta reconectarse si no hay conexión o la conexión está cerrada
-
-
         # Envía el mensaje a la cola de RabbitMQ
-        channel.basic_publish(exchange='', routing_key='steven', body=message)
-
-        emit('message', message, broadcast=True)  # Envía el mensaje a los clientes conectados
-
+        channel.basic_publish(exchange='', routing_key=enviarA, body=mensaje)
     except pika.exceptions.AMQPConnectionError as e:
         
         print("Mensaje de error real:", str(e))
 
-
-def start_consuming():
-
-    try:
-        print("Consumiendo mensajes...")
-        channelRecibir.basic_consume(queue='alexander', on_message_callback=callback, auto_ack=True)
-        channelRecibir.start_consuming()
-    except pika.exceptions.AMQPConnectionError as e:
-        print("Error de conexión RabbitMQ:", str(e))
-            # Intenta reconectarse
-        connect_rabbitmqRecibir()
+@socketio.on('pedirMensajes')
+def handle_recibir(perfil):
+    if perfil != None:
+        method_frame, _, body = channel.basic_get(queue=perfil, auto_ack=True)
+        if method_frame:
+            emit('recibir', body.decode(), broadcast=False)
+        else:
+            pass
+    else:
+        pass
 
 if __name__ == '__main__':
     socketio.run(app)
--- static/js/script.js
+++ static/js/script.js
@@ -1,6 +1,21 @@
+// Obtener el elemento select del ComboBox
+var select = document.getElementById("animal-select");
+var enviar = 'Leon';
+
+// Obtener los elementos de la cabecera a actualizar
+var animalImg = document.getElementById("animal-img");
+var chatTitle = document.getElementById("chat-title");
+
+// Agregar un evento de cambio al select
+select.addEventListener("change", function() {
+  // Obtener el valor seleccionado
+  enviar = select.value;
+});
+
+
 var socket = io();
+
 let numMensajes = 0;
-var idUser = null;
 
 // Función para mostrar el modal al cargar la página
 document.addEventListener('DOMContentLoaded', function() {
@@ -11,19 +26,32 @@ document.addEventListener('DOMContentLoaded', function() {
     $('#modalID').modal('show');
 });
 
-function guardarID() {
+let idUser = null;
 
-    var inputID = document.getElementById('inputID');
-    var idIngresado = inputID.value.trim();
+function guardarID(nombrePerfil) {
+    idUser = nombrePerfil;
+    socket.emit('usuario',idUser);
+    console.log("Perfil seleccionado:", idUser);
+    
+    switch (idUser) {
+        case "Leon":
+          animalImg.src = "../static/images/leon.png";
+          chatTitle.textContent = "León (Server 2)";
+          break;
+        case "Mapache":
+          animalImg.src = "../static/images/mapache.png";
+          chatTitle.textContent = "Mapache (Server 2)";
+          break;
+        case "Zorro":
+          animalImg.src = "../static/images/zorro.png";
+          chatTitle.textContent = " Zorro (Server 2)";
+          break;
+      }
 
-    if (idIngresado !== "") {
-        idUser = idIngresado;
-        console.log("ID ingresado:", idUser);
-
-        // Cierra el modal después de guardar el ID
-        $('#modalID').modal('hide');
-    }
+    $('#modalID').modal('hide');
 }
+
+
 
 function fecha(){
     var fechaActual = new Date();
@@ -46,30 +74,18 @@ function horaH(){
 //Función para el envío de mensajes para el texto del textarea
 function sendMessage() {
     var message = document.getElementById('message').value;
+    var fech = fecha();
+    var hor = horaH();
     if (message.trim() !== '') {
-        mensaje = message+'@'+fecha()+ '@'+ horaH() + '@' + idUser;
-        socket.emit('message', mensaje); //Envía a la cola
+
+        mensaje = message+'@'+ fech + '@'+ hor + '@' + idUser;
+
+
         document.getElementById('message').value = '';
         document.getElementById('message').focus();
-    }
-}
 
-socket.on('connect', function() {
-    console.log('Connected');
-  });
-
-  socket.on('disconnect', function() {
-    console.log('Disconnected');
-  });
-
-//WebSocket para escuchar los mensajes recibidos
-socket.on('message', function(message) {
-    numMensajes ++;
-    const numeroElemento = document.getElementById('numero');
-    numeroElemento.textContent = numMensajes + ' Mensajes';
-    //Descomponer el mensaje
-    var datos = message.split('@');
-    if (datos[3] == idUser) {
+        let datos = mensaje.split('@');
+        
         console.log("Mensaje Enviado");
         var chatBox = document.getElementById('chat-box');
         var messageContainer = document.createElement('div');
@@ -79,13 +95,18 @@ socket.on('message', function(message) {
         textContainer.classList.add('msg_cotainer_send');
         textContainer.textContent = datos[0];
 
+        var nameElement = document.createElement('span');
+        nameElement.classList.add('msg_name');
+        nameElement.textContent = 'Enviado a '+ enviar;
+
         var timeElement = document.createElement('span');
         timeElement.classList.add('msg_time_send');
-        timeElement.textContent = datos[2]+', ' + datos[1]; // Aquí puedes agregar la hora actual del mensaje
+        timeElement.textContent = datos[2] +', ' + datos[1]; // Aquí puedes agregar la hora actual del mensaje
 
         var imgContainer = document.createElement('div');
         imgContainer.classList.add('img_cont_msg');
 
+        textContainer.appendChild(nameElement);
         var imgElement = document.createElement('img');
         imgElement.src = "../static/images/enviado.png";
         imgElement.classList.add('user_img_msg', 'img_no_border');
@@ -97,40 +118,68 @@ socket.on('message', function(message) {
         chatBox.appendChild(messageContainer);
 
         chatBox.scrollTop = chatBox.scrollHeight;
-    } else {
-        console.log("Mensaje Recibido");
-        var chatBox = document.getElementById('chat-box');
-        var messageContainer = document.createElement('div');
-        messageContainer.classList.add('d-flex', 'justify-content-start', 'mb-4');
 
-        var imgContainer = document.createElement('div');
-        imgContainer.classList.add('img_cont_msg');
-
-        var imgElement = document.createElement('img');
-        imgElement.src = "../static/images/recibido.png";
-        imgElement.classList.add('user_img_msg', 'img_no_border');
-
-        var nameElement = document.createElement('span');
-        nameElement.classList.add('msg_name');
-        nameElement.textContent = datos[3];
-
-        var textContainer = document.createElement('div');
-        textContainer.classList.add('msg_cotainer');
-        textContainer.textContent = datos[0];
-
-        var timeElement = document.createElement('span');
-        timeElement.classList.add('msg_time');
-        timeElement.textContent = datos[2]+', ' + datos[1]; // Aquí puedes agregar la hora actual del mensaje
-
-        textContainer.appendChild(nameElement);
-        textContainer.appendChild(timeElement);
-        imgContainer.appendChild(imgElement);
-        messageContainer.appendChild(imgContainer);
-        messageContainer.appendChild(textContainer);
-        chatBox.appendChild(messageContainer);
-
-        chatBox.scrollTop = chatBox.scrollHeight;
+        socket.emit('message', {'profile': idUser, 'enviarA': enviar, 'message': mensaje}); //Envía a la cola
     }
+}
+
+socket.on('connect', function() {
+    console.log('Conectado al servidor');
+});
+
+socket.on('disconnect', function() {
+    console.log('Desconectado del servidor');
+});
+
+
+setInterval(function() {
+console.log("Enviando");
+socket.emit('pedirMensajes', idUser);
+}, 1000);
+
+//WebSocket para escuchar los mensajes recibidos
+socket.on('recibir', function(message) {
+    // Case para mostrar el mensaje recibido
+    console.log("Escuchando");
+    console.log(message);
+    numMensajes ++;
+    const numeroElemento = document.getElementById('numero');
+    numeroElemento.textContent = numMensajes + ' Mensajes';
+    //Descomponer el mensaje
+    var datos = message.split('@');
+    
+    console.log("Mensaje Recibido");
+    var chatBox = document.getElementById('chat-box');
+    var messageContainer = document.createElement('div');
+    messageContainer.classList.add('d-flex', 'justify-content-start', 'mb-4');
+
+    var imgContainer = document.createElement('div');
+    imgContainer.classList.add('img_cont_msg');
+
+    var imgElement = document.createElement('img');
+    imgElement.src = "../static/images/recibido.png";
+    imgElement.classList.add('user_img_msg', 'img_no_border');
+
+    var nameElement = document.createElement('span');
+    nameElement.classList.add('msg_name');
+    nameElement.textContent = datos[3];
+
+    var textContainer = document.createElement('div');
+    textContainer.classList.add('msg_cotainer');
+    textContainer.textContent = datos[0];
+
+    var timeElement = document.createElement('span');
+    timeElement.classList.add('msg_time');
+    timeElement.textContent = datos[2]+', ' + datos[1]; // Aquí puedes agregar la hora actual del mensaje
+
+    textContainer.appendChild(nameElement);
+    textContainer.appendChild(timeElement);
+    imgContainer.appendChild(imgElement);
+    messageContainer.appendChild(imgContainer);
+    messageContainer.appendChild(textContainer);
+    chatBox.appendChild(messageContainer);
+
+    chatBox.scrollTop = chatBox.scrollHeight;
 });
 
 document.getElementById('send-button').addEventListener('click', sendMessage);
